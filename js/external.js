f0c274735c62eb65ac469006ce99e6d6ec9c72fa
Chanelle Ramirez <chanelleramirez13@gmail.com>
2022-10-06 10:39:30 -0500

    first commit

---
+++ js/external.js
@@ -0,0 +1,26 @@
+"use strict";
+
+console.log('Hello from JavaScript!');
+
+alert("Good Morning Xerxes");
+
+// the following line will show the OK/CANCEL confirm dialog
+var confirmed = confirm('Are you sure you want to do XYZ?');
+console.log(confirmed); // will be either true or false
+
+var userInput = prompt('Please type something:');
+console.log('The user entered: ' + userInput);
+
+console.log('Hello from JavaScript!');
+
+alert('Welcome to my Website!');
+
+var userInput = prompt('Please enter your favorite color:');
+console.log('the color you entered is my favorite color too: ' + userInput);
+
+var userInput = prompt('Please use a number:');
+console.log('Number entered: ' + userInput);
+
+alert("Information you entered.");
+
+
