8930c124d0711b6bf05a34c3ce98f0cb86743472
Chanelle Ramirez <chanelleramirez13@gmail.com>
2022-10-14 14:40:00 -0500

    for loop exercises

---
+++ js/for_loops.js
@@ -0,0 +1,24 @@
+"use strict"
+//2.
+//function showMultiplicationTable(num){
+    //for (var i = 1;  i <= 10; i++) {
+       // console.log(num + ' x ' + i + ' = ' + (num * i))
+    //}
+//}
+//showMultiplicationTable(7)
+//showMultiplicationTable(13)
+
+
+//3.
+function random() {
+    for (var i = 20; i <= 30; i++) {
+        if (i % 2 == 0) {
+            console.log(' Even numbers only ' + i );
+        }
+            else {
+            console.log(' Odd numbers only '  + i  );
+        }
+    }
+}
+random('Even numbers only') + ('Odd numbers only')
+
