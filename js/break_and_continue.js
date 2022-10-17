6f338205f03aa77030aacc8687ed24245cc389f1
Chanelle Ramirez <chanelleramirez13@gmail.com>
2022-10-17 12:09:18 -0500

    complete break_and_continue exercise 2

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,15 +1,25 @@
 "use strict"
 
-1.
-do {
-    let oddNumber = prompt("Please enter an odd number between 1 and 50");
-    {
-        if (oddNumber % 2 === 0){
-            alert('The number you entered was even. Please try again.');
-        } else if('The number you entered is odd');
-        break;
+//1.
+//do {
+    //let oddNumber = prompt("Please enter an odd number between 1 and 50");
+    //{
+        //if (oddNumber % 2 === 0){
+          //  alert('The number you entered was even. Please try again.');
+        //} else if('The number you entered is odd');
+       // break;
 
+   // }
+//} while(true)
+
+2.
+let oddNumber = prompt("Please enter an odd number between 1 and 50");
+for (let i = 1; i <= 50; i++){
+    if (oddNumber == i){
+        console.log('Skipping number'  + oddNumber);
+        continue;
     }
-} while(true)
+    console.log('Hey there odd number' + i);
+}
 
 
