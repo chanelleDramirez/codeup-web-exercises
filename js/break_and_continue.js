3c49084e355a0ffb09c4f0aa45a1d51e85ff6a33
Chanelle Ramirez <chanelleramirez13@gmail.com>
2022-10-17 14:51:00 -0500

    complete Array exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -2,24 +2,24 @@
 
 //1.
 //do {
-    //let oddNumber = prompt("Please enter an odd number between 1 and 50");
+  //  let oddNumber = prompt("Please enter an odd number between 1 and 50");
     //{
-        //if (oddNumber % 2 === 0){
-          //  alert('The number you entered was even. Please try again.');
+      //  if (oddNumber % 2 === 0){
+        //    alert('The number you entered was even. Please try again.');
         //} else if('The number you entered is odd');
-       // break;
+       //break;
 
-   // }
+    //}
 //} while(true)
 
-2.
-let oddNumber = prompt("Please enter an odd number between 1 and 50");
-for (let i = 1; i <= 50; i++){
-    if (oddNumber == i){
-        console.log('Skipping number'  + oddNumber);
-        continue;
-    }
-    console.log('Hey there odd number' + i);
-}
+//2.
+//let oddNumber = prompt("Please enter an odd number between 1 and 50");
+//for (let i = 1; i <= 50; i++){
+   // if (oddNumber == i){
+      //  console.log('Skipping number'  + oddNumber);
+      //  continue;
+   // }
+   // console.log('Hey there odd number' + i);
+//}
 
 
---
+++ js/while.js
@@ -0,0 +1,6 @@
+{
+     var i = 1;
+    while (i < 65536){
+    }
+    console.log(i *= 2);
+}
