219968f961f13cb95cda90ed81c8bed98999be4a
Chanelle Ramirez <chanelleramirez13@gmail.com>
2022-10-17 23:13:47 -0500

    completed while exercise

--- js/while.js
+++ js/while.js
@@ -1,6 +1,18 @@
-{
-     var i = 1;
-    while (i < 65536){
+"use strict"
+
+var allCones = Math.floor(Math.random() * 50) + 50;
+
+do {
+
+    var conesToBuy = Math.floor(Math.random() * 5) + 1;
+
+    if((allCones - conesToBuy) < 0){
+
+        console.log("Cannot sell you " + conesToBuy + allCones + "...");
+    }else{
+    console.log("# of cones before purchase:" + allCones);
+    allCones = conesToBuy;
+    console.log(conesToBuy + "cones sold...");
     }
-    console.log(i *= 2);
-}
+
+}while(allCones > 0);
