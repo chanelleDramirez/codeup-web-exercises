aca02cd09837eed44678f01c20f539c9691bf12b
Chanelle Ramirez <chanelleramirez13@gmail.com>
2022-10-21 09:36:02 -0500

    Function and Array warmup

--- js/warmup.js
+++ js/warmup.js
@@ -1,5 +1,13 @@
 "use strict"
 
-function shoes(){
-    let shoes = {"walking", quantity: 3, price: "$120"};
-}
+//Write a function that when passed an object will return the value of the object's price property. Example: let obj = {name: "Potatoes", quantity: 2, price: "$7.89"}; get price(obj); //returns "$7.89"
+
+
+//function shoes(){
+  //  let shoes = {"walking", quantity: 3, price: "$120"};
+//}
+
+//Write a function that when passed an array will return the longest string from the array.Example const arr = ["Green", "Blue", "Periwinkle"]; getLongestString(arr)// Returns "Periwinkle"
+
+function sports = ["Soccer", "Baseball", "Football", "Basketball"];
+console.log(sports.length);
