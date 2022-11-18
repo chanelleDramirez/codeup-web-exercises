ea29bc2eaa5b05b9275166355b8a3bc34ec38824
Chanelle Ramirez <chanelleramirez13@gmail.com>
2022-11-18 15:25:02 -0600

    Completed Mouse Events exercise

--- jquery.js
+++ jquery.js
@@ -1,13 +1,17 @@
 "use strict"
 $(function() {
-    // alert( 'The DOM has finished loading!' );
-    // alert("#welcome page");
-    // alert("#Read up on the Keys!");
-    // $(".codeup").css("border", "1px solid red");
-    $("li").css("font-size" ,"20px");
-   // $("h1").css("background","green");
-    //("p").css("background","lavender");
-    //("li").css("background","violet");
-    alert("#Another Path to Education!");
-    $("h1, p, li").css("background-color", "pink");
+  $("h1").click(function(){
+      $(this).css("background", "orange");
+  });
+
+  $("p").dblclick(function() {
+      $(this).css("font-size", "18px");
+
+  });
+
+  $("li").hover(function(){
+      $(this).css("color", "red");
+  }, function () {
+      $(this).css("color", "black");
+  });
 });
