ec2789c200466e3dc1fd20db711c0e14ac329292
Chanelle Ramirez <chanelleramirez13@gmail.com>
2022-11-18 12:03:08 -0600

    Element selectors exercise

--- jquery.js
+++ jquery.js
@@ -3,5 +3,6 @@ $(function() {
     // alert( 'The DOM has finished loading!' );
     // alert("#welcome page");
     // alert("#Read up on the Keys!");
-    $(".codeup").css("border", "1px solid red");
+    // $(".codeup").css("border", "1px solid red");
+    $("li").css("font-size" ,"20px");
 });
