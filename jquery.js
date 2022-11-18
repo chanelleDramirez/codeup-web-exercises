96cc6eb50a696add656d7f24fd6ecf791c72df56
Chanelle Ramirez <chanelleramirez13@gmail.com>
2022-11-18 11:55:27 -0600

    Class selectors exercise

--- jquery.js
+++ jquery.js
@@ -1,5 +1,7 @@
+"use strict"
 $(function() {
     // alert( 'The DOM has finished loading!' );
     // alert("#welcome page");
     // alert("#Read up on the Keys!");
+    $(".codeup").css("border", "1px solid red");
 });
