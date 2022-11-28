a38c74092d1aa9b33e26ec988f24beddb0f0732d
Chanelle Ramirez <chanelleramirez13@gmail.com>
2022-11-28 15:33:25 -0600

    Completed Traverse exercise

--- js/jQuery_faq.js
+++ js/jQuery_faq.js
@@ -6,3 +6,22 @@ $(document).ready(function() {
         $('dd').toggleClass('invisible');
     });
 });
+
+
+$("dt").click(function() {
+    $(this).toggleClass("highlighter");
+});
+
+$("#btn-2").click(function() {
+    $("ul").each(function () {
+        $(this).children().last().toggleClass("yellow-bg");
+    });
+});
+
+$("h3").click(function () {
+    $(this).next().children().css("font-weight", "bold");
+});
+
+$("li").click(function() {
+    $(this).parent().children().first().css("color", "blue");
+});
