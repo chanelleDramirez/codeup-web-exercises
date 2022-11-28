b4f012282080338335013a4ada635f80330370b5
Chanelle Ramirez <chanelleramirez13@gmail.com>
2022-11-28 15:36:22 -0600

    Completed Attributes and CSS exercise

--- js/jQuery_faq.js
+++ js/jQuery_faq.js
@@ -12,16 +12,16 @@ $("dt").click(function() {
     $(this).toggleClass("highlighter");
 });
 
-$("#btn-2").click(function() {
-    $("ul").each(function () {
-        $(this).children().last().toggleClass("yellow-bg");
-    });
-});
-
-$("h3").click(function () {
-    $(this).next().children().css("font-weight", "bold");
-});
-
-$("li").click(function() {
-    $(this).parent().children().first().css("color", "blue");
-});
+// $("#btn-2").click(function() {
+//     $("ul").each(function () {
+//         $(this).children().last().toggleClass("yellow-bg");
+//     });
+// });
+//
+// $("h3").click(function () {
+//     $(this).next().children().css("font-weight", "bold");
+// });
+//
+// $("li").click(function() {
+//     $(this).parent().children().first().css("color", "blue");
+// });
