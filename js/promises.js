e43ef60ef1fa4cdedd7acfa6a2220195b1320fea
Chanelle Ramirez <chanelleramirez13@gmail.com>
2022-12-08 14:15:56 -0600

    promises exercise

---
+++ js/promises.js
@@ -0,0 +1,18 @@
+"use strict";
+
+(() => {
+    fetch( {headers: {'Authorization': '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'}})
+
+    fetch('https://api.github.com/users')
+        .then(response => response.json())
+        .then(users => {
+            users.forEach( "sb-son", "deramusWyatt", "Jonathanvra"); {
+                // do something with each username
+                console.log(users);
+            }
+        })
+        .catch(error => console.error(error));
+
+
+
+}) ();
