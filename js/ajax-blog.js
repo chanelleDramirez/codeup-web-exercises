8aad1d7de0e0d1180026838064a34e4d5da22f07
Chanelle Ramirez <chanelleramirez13@gmail.com>
2022-12-16 13:44:51 -0600

    Completed exercise

--- js/ajax-blog.js
+++ js/ajax-blog.js
@@ -1,22 +1,24 @@
 "use strict";
 
-// $(function() {
-//     $.get('/blog.json').done(function(data) {
-//
-//         data.while(function(post){
-//
-//         }
-//             console.log();
-//             i++;
-//
-// });
 
-$(function(){
-    $.get("data/blog.json").append(content,function(data){
-
+(function(){
+    $.ajax("../data/blog.json").done(function(data) {
+        console.log(data);
+        let list = data;
+        for (let i = 0; i < list.length; i++) {
+            let blogList = `<h3>${list[i].title}</h3>
+             <p>${list[i].content}</p>
+             <p>${list[i].date}</p>
+             <p class="categories">${list[i].categories.join(',')}</p>`
+            $(`#posts`).append(blogList)
+        }
     })
-    console.log(data.title);
-    console.log(data.content);
-    console.log(data.categories);
-    console.log(data.date);
-})
+})();
+
+
+
+
+
+
+
+
